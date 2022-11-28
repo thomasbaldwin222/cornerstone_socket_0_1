8afa062c052f31e790daa96e58cda7fa2bdf47bf
Thomas Baldwin <thomasbaldwin222@gmail.com>
2022-11-28 06:58:46 -0600

    c

--- index.js
+++ index.js
@@ -1,70 +1,40 @@
-var socket = io.connect("http://10.0.0.217:3001", {
+// TODO
+// set maximum socket conection to DB middleware, spin up another middleware when connection count reaches limit
+// Handle disconnect
+// Send user location data
+
+const CONNECTION_URL = "http://10.0.0.217:3001";
+var socket = io.connect(CONNECTION_URL, {
   secure: true,
   rejectUnauthorized: false,
   withCredentials: true,
   transport: ["websocket"],
 });
 
-// rrwebRecord({
-//   emit(event) {
-//     console.log("event", event)
-//   },
-// });
+// function debounce(func, timeout = 15) {
+//   let timer;
+//   return (...args) => {
+//     clearTimeout(timer);
+//     timer = setTimeout(() => {
+//       func.apply(this, args);
+//     }, timeout);
+//   };
+// }
 
-function debounce(func, timeout = 15) {
-  let timer;
-  return (...args) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      func.apply(this, args);
-    }, timeout);
-  };
-}
-function saveInput() {
-  console.log("Saving data");
-}
-const processChange = debounce(() => saveInput());
+console.log("_socket: intiialized");
+console.log("_socket: attempting connection...");
 
 //Listener
 socket.on("connect", () => {
   let sessionId = null;
-  console.log("connecting attempt");
+  let previousUrl = "";
+
+  console.log({ socket });
+
   try {
-    console.log("Successfuly conntected to socket @ http://10.0.0.217:3001");
+    console.log("_socket: connection sucesss, listening at " + CONNECTION_URL);
 
-    socket.on("create_session", (session) => {
-      sessionId = session.id;
-    });
-
-    rrwebRecord({
-      emit(event) {
-        socket.emit("rrweb_event", event);
-      },
-    });
-
-    let previousUrl = "";
-
-    const urlObserver = () => {
-      if (window.location.href !== previousUrl) {
-        socket.emit("packet", [
-          {
-            type: "navigate",
-            date: Date.now(),
-            data: {
-              url: window.location.href,
-            },
-          },
-        ]);
-        previousUrl = window.location.href;
-      }
-    };
-
-    const observer = new MutationObserver(urlObserver);
-    const config = { subtree: true, childList: true };
-    // start observing change
-    observer.observe(document, config);
-    // window.addEventListener("mousemove", onMouseMove);
-
+    // Create session through middleare
     socket.emit("create_session", {
       company_id: 1,
       screen: {
@@ -78,4 +48,35 @@ socket.on("connect", () => {
   } catch (e) {
     console.log(e);
   }
+
+  socket.on("create_session", (session) => {
+    sessionId = session.id;
+  });
+
+  // Initialize rrweb recorder
+  rrwebRecord({
+    emit(event) {
+      socket.emit("rrweb_event", event);
+    },
+  });
+
+  // Create mutation observer to listen to url changes
+  const observer = new MutationObserver(urlObserver);
+  const config = { subtree: true, childList: true };
+  observer.observe(document, config);
 });
+
+const urlObserver = () => {
+  if (window.location.href !== previousUrl) {
+    socket.emit("packet", [
+      {
+        type: "navigate",
+        date: Date.now(),
+        data: {
+          url: window.location.href,
+        },
+      },
+    ]);
+    previousUrl = window.location.href;
+  }
+};
